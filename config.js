26b711a096d02f67f68a8942563a93c9401bdf07
Andreas Dewes <andreas.dewes@7scientists.com>
2018-05-27 01:13:54 +0200

    docs -> dist.

---
+++ config.js
@@ -0,0 +1,49 @@
+window.cookieConfig = {
+    privacyPolicy: '/privacy.html',
+    apps : [
+        {
+            name : 'google-analytics',
+            title : 'Google Analytics',
+            purposes : ['statistics'],
+            description : {
+                de : 'Besucherstatistiken',
+                en : 'Visitor Statistics',
+            },
+        },
+        {
+            name : 'matomo',
+            title : 'Matomo/Piwik',
+            description : {
+                de : 'Besucherstatistiken',
+                en : 'Visitor Statistics',
+            },
+        },
+        {
+            name : 'intercom',
+            title : 'Intercom',
+            description : {
+                de : 'Chat Widget & Besucherstatistiken',
+                en : 'Chat Widget & Visitor Statistics',
+            },
+        },
+        {
+            name : 'mouseflow',
+            title : 'Mouseflow',
+            description : {
+                de : 'Echtzeit-Benutzeranalyse',
+                en : 'Real-Time User Analytics',
+            },
+            cookies : [/mouseflow/i],
+        },
+    ],
+    purposes: {
+        statistics : {
+            en : 'Statistics',
+            de : 'Statistiken',
+        },
+        security : {
+            de : 'Sicherheit',
+            en : 'Security',
+        }
+    },
+}
